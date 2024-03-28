be2ba9e600a2d1cd8f4b1feb0dbd777e9a4f97d1
Alireza <siralirezagh@gmail.com>
2024-03-28 12:07:49 -0400

    feature(support-polars-df): Add Tests for the support of polars dataframe.

--- tests/encoding/test_trace_encoding.py
+++ tests/encoding/test_trace_encoding.py
@@ -1,9 +1,10 @@
+import polars as pl
 import pytest
 import numpy as np
 import pandas as pd
-import datetime as dt
 from skpm.encoding import Aggregation, WindowAggregation
 from skpm.config import EventLogConfig as elc
+
 
 def test_aggregation():
     dummy_data = pd.DataFrame(
@@ -32,6 +33,21 @@ def test_aggregation():
     with pytest.raises(Exception) as exc_info:
         rp.transform(dummy_data[[elc.activity, elc.resource]])
 
+
+def test_aggregation_with_polars():
+    pl_df = pl.DataFrame({
+        elc.case_id: np.repeat(np.arange(0, 10), 100),
+        elc.activity: np.random.randint(0, 10, 1000),
+        elc.resource: np.random.randint(0, 3, 1000),
+    })
+
+    rp = Aggregation(engine="polars").set_output(transform="polars")
+    rp.fit(pl_df)
+    out = rp.transform(pl_df)
+    assert isinstance(out, pl.DataFrame)
+    assert out.height == pl_df.height
+
+
 def test_window():
     n = 100
     dummy_data = pd.DataFrame(
@@ -59,3 +75,29 @@ def test_window():
     out = rp.transform(dummy_data)
     assert isinstance(out, pd.DataFrame)
     assert out.shape[0] == dummy_data.shape[0]
+
+
+def test_window_with_polars():
+    n = 100
+    pl_df = pl.DataFrame(
+        {
+            elc.case_id: np.random.choice(np.arange(0, 10), n),
+            elc.activity + "_oh_1": np.random.randint(0, 2, n),
+            elc.activity + "_oh_2": np.random.randint(0, 2, n),
+            elc.resource + "_oh_1": np.random.randint(0, 3, n),
+            elc.resource + "_oh_2": np.random.randint(0, 3, n),
+            elc.resource + "_oh_3": np.random.randint(0, 3, n),
+            "somehow_transformed_timestamp": np.random.rand(n),
+        }
+    )
+    # Test windowed aggregation
+    rp = WindowAggregation(window_size=3,num_method="sum", cat_method="sum", engine="polars").set_output(transform="polars")
+    rp.fit(pl_df)
+    out = rp.transform(pl_df)
+    assert isinstance(out, pl.DataFrame)
+    assert out.height == pl_df.height
+
+
+def compare_output():
+    # TODO
+    pass
