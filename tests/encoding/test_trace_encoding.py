0f4fe3b6dac45602ce3779bd5d0c4af93a254c86
Alireza <siralirezagh@gmail.com>
2024-03-28 13:39:51 -0400

    feature(support-polars-df): Add Tests to compare results of both polars and pandas dataframes.

--- tests/encoding/test_trace_encoding.py
+++ tests/encoding/test_trace_encoding.py
@@ -91,13 +91,49 @@ def test_window_with_polars():
         }
     )
     # Test windowed aggregation
-    rp = WindowAggregation(window_size=3,num_method="sum", cat_method="sum", engine="polars").set_output(transform="polars")
+    rp = WindowAggregation(window_size=3, num_method="sum", cat_method="sum", engine="polars").set_output(
+        transform="polars")
     rp.fit(pl_df)
     out = rp.transform(pl_df)
     assert isinstance(out, pl.DataFrame)
     assert out.height == pl_df.height
 
 
-def compare_output():
-    # TODO
-    pass
+def test_aggregation_output():
+    pl_df = pl.DataFrame(
+        {
+            elc.case_id: np.repeat(np.arange(0, 10), 100),
+            elc.activity: np.random.randint(0, 10, 1000),
+            elc.resource: np.random.randint(0, 3, 1000),
+        }
+    )
+    pd_df = pl_df.to_pandas()
+
+    agg1 = Aggregation(num_method="sum").set_output(transform="pandas")
+    pd_agg = agg1.fit_transform(pd_df)
+    agg2 = Aggregation(num_method="sum", engine="polars").set_output(transform="pandas")
+    pl_agg = agg2.fit_transform(pl_df)
+    assert isinstance(pl_agg, pd.DataFrame)
+    assert pd_agg.equals(pl_agg)
+
+
+def test_window_output():
+    pl_df = pl.DataFrame(
+        {
+            elc.case_id: np.repeat(np.arange(0, 10), 100),
+            elc.activity: np.random.randint(0, 10, 1000),
+            elc.resource: np.random.randint(0, 3, 1000),
+        }
+    )
+    pd_df = pl_df.to_pandas()
+
+    agg1 = WindowAggregation(window_size=3, num_method="sum", cat_method="sum", engine="pandas").set_output(
+        transform="pandas")
+    pd_agg = agg1.fit_transform(pd_df)
+    agg2 = WindowAggregation(window_size=3, num_method="sum", cat_method="sum", engine="polars").set_output(
+        transform="pandas")
+    pl_agg = agg2.fit_transform(pl_df)
+    print("pd:\n", pd_agg.head(15))
+    print("pl:\n", pl_agg.head(15))
+    assert isinstance(pl_agg, pd.DataFrame)
+    assert pd_agg.equals(pl_agg)
