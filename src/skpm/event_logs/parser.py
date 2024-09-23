06c9aba637b2a5797958e7a56ee01dc1e373dc8a
raseidi <rso.seidi@gmail.com>
2024-09-23 23:23:05 +0200

    xes parser

--- src/skpm/event_logs/parser.py
+++ src/skpm/event_logs/parser.py
@@ -2,13 +2,89 @@ from lxml import etree
 from itertools import chain
 from joblib import Parallel, delayed
 from typing import Generator
+import pandas as pd
+
+DataFrame = pd.DataFrame
 
 
 class Event(dict):
     pass
 
+class TagXES:
+    # attributes
+    STRING: str = "string"
+    DATE: str = "date"
+    FLOAT: str = "float"
+    BOOLEAN: str = "boolean"
+    INT: str = "int"
+    
+    # elements
+    EVENT: str = "event"
+    TRACE: str = "trace"
+    
+    _DTYPES: tuple = (STRING, DATE, FLOAT, BOOLEAN, INT)
+    
+    @classmethod
+    def is_attribute(cls, element: etree._Element) -> bool:
+        """element is an attribute if it ends with one of the dtypes."""
+        return element.tag.endswith(cls._DTYPES)
+    
+    @classmethod
+    def is_valid(cls, element: etree._Element) -> bool:
+        return element.tag.endswith(tuple(v for v in vars(cls).values() if not v.startswith("_")))
+    
+    @classmethod
+    def get_dtypes(cls) -> tuple:
+        return cls._DTYPES
 
-def parse_trace(trace: list[etree._Element]) -> list[Event]:
+tag: TagXES = TagXES
+
+def extract_case_attributes(trace: etree._Element, ns: dict) -> Event:
+    """
+    Extracts case-level attributes from the trace.
+    
+    Using findall for case attributes is faster than using iter since
+    cases has fewer attributes than events.
+
+    Args:
+        trace (etree._Element): The trace element.
+        ns (dict): Namespace mapping for XML parsing.
+
+    Returns:
+        Event: A dictionary of case-level attributes.
+    """
+    case_attrs = Event()
+    for attr in tag.get_dtypes():
+        # Find all attributes of the given type in the trace
+        attrs = trace.findall(attr, ns)
+        # Update case_attrs with the found attributes
+        case_attrs.update({
+            f'case:{e.get("key")}': e.get("value") for e in attrs
+        })
+    return case_attrs
+
+
+def extract_event_attributes(event: etree._Element) -> Event:
+    """
+    Extracts attributes from an event element.
+    
+    Using iter is slightly faster than findall for events since
+    there many events and event attributes in a trace.
+
+    Args:
+        event (etree._Element): The event element.
+
+    Returns:
+        Event: A dictionary of event attributes.
+    """
+    event_attrs = Event()
+    for e_attr in event.iter():
+        if tag.is_attribute(e_attr):
+            event_attrs[e_attr.get("key")] = e_attr.get("value")
+    return event_attrs
+
+
+def parse_trace(trace: list[etree._Element], ns: dict) -> list[Event]:
     """Parses a list of XML elements representing a trace.
 
     Args:
@@ -21,36 +97,34 @@ def parse_trace(trace: list[etree._Element]) -> list[Event]:
     if type(trace) == bytes:
         trace = etree.fromstring(trace)
 
-    case_attrs = Event()
-    for attr in ["string", "date", "float", "boolean", "int"]:
-        case_attrs.update(
-            {f'case:{e.get("key")}': e.get("value") for e in trace.findall(attr)}
-        )
-
-    events = trace.findall("event")
-    final_events = []
+    case_attrs = extract_case_attributes(trace, ns)
+    
+    # Parse each event
+    parsed_events = []
+    events = trace.findall(tag.EVENT, ns)
     for event in events:
-        event_attrs = Event()
-        for e in event.iter():
-            if e.tag in ["string", "date", "float", "boolean", "int"]:
-                event_attrs[e.get("key")] = e.get("value")
-        event.clear()
+        event_attrs = extract_event_attributes(event)
+        
+        # Add case-level attributes to event attributes
         event_attrs.update(case_attrs)
-        final_events.append(event_attrs)
+        parsed_events.append(event_attrs)
+        
+        # Clear the event to free memory
+        event.clear()
 
     trace.clear()
-    return final_events
+    return parsed_events
 
 
-def lazy_serialize(elements: list[etree._Element]) -> Generator[bytes]:
+def lazy_serialize(elements: list[etree._Element]) -> Generator[bytes, None, None]:
     """Lazy serialization of a list of XML elements. Used for parallel processing."""
     for element in elements:
         yield etree.tostring(element)
 
 
-def read_from_xes(filepath: str, n_jobs: int = None) -> list[Event]:
+def read_xes(filepath: str, n_jobs: int = None, as_df=True) -> DataFrame | list[Event]:
     """Reads an event log from a XES file.
-    
+
     Rough overview:
         This function reads an event log from a XES file. It uses the lxml library to
         parse the XML file. The function is parallelized using the joblib library.
@@ -61,7 +135,7 @@ def read_from_xes(filepath: str, n_jobs: int = None) -> list[Event]:
         4. Update the event attributes with the case attributes.
         5. Append the event to the final list of events corresponding to the trace.
         6. Return trace and repeat.
-    
+
     Args:
         filepath (str): Filepath to the XES file.
         n_jobs (int, optional): Number of CPU cores to use. If None, only one core
@@ -70,15 +144,22 @@ def read_from_xes(filepath: str, n_jobs: int = None) -> list[Event]:
     Returns:
         list[Event]: an event log as a list of Event objects.
     """
-    tree = etree.parse(filepath)
-    traces = tree.findall("trace")
+    tree = etree.parse(filepath).getroot()
+    ns = tree.nsmap
+    
+    
+    traces = tree.findall(tag.TRACE, ns)
 
     if n_jobs in [1, None]:
         log = []
         for trace in traces:
-            log.extend(parse_trace(trace))
+            log.extend(parse_trace(trace, ns))
     else:
         traces = lazy_serialize(traces)
         log = Parallel(n_jobs=n_jobs)(delayed(parse_trace)(trace) for trace in traces)
         log = list(chain(*log))
+        
+    if as_df:
+        log = pd.DataFrame(log)
+        
     return log
