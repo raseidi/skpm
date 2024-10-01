3f3caf9d9a540d6ac03c1c09198bb5e379128f3c
raseidi <rso.seidi@gmail.com>
2024-10-01 17:26:42 +0200

    Add sphinx gallery config for tutorials

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,14 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+from sphinx_gallery.sorting import FileNameSortKey
+from pathlib import Path
+
+# find project
+sys.path.insert(0, str(Path(__file__).parents[1]))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -15,11 +23,12 @@ release = "0.0.1"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "myst_nb",
-    "autoapi.extension",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-]
+    'sphinx.ext.autodoc',
+    'sphinx_gallery.gen_gallery',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    ]
+
 autoapi_dirs = ["../src"]
 
 templates_path = ["_templates"]
@@ -31,3 +40,20 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+sg_examples_dir = "../examples"
+sg_gallery_dir = "auto_examples"
+sphinx_gallery_conf = {
+    # path to your example scripts
+    'examples_dirs': [sg_examples_dir],
+    # path to where to save gallery generated output
+    'gallery_dirs': [sg_gallery_dir],
+    
+    # specify that examples should be ordered according to filename
+    'within_subsection_order': FileNameSortKey,
+    # directory where function granular galleries are stored
+    'backreferences_dir': 'gen_modules/backreferences',
+    # Modules for which function level galleries are created.  In
+    # this case sphinx_gallery and numpy in a tuple of strings.
+    'doc_module': ('SampleModule'),
+}
