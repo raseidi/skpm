e69d1575e57a4f1f21fc610af335cd4332297908
raseidi <rso.seidi@gmail.com>
2024-10-04 20:43:07 +0200

    log download done and unbiased split draft

---
+++ examples/unbiased_split.py
@@ -0,0 +1,16 @@
+
+"""
+Unbiased Split of Event Logs
+============================
+
+This example.
+"""
+
+# %%
+# The `unbiased` function
+# -----------------------
+
+from skpm.event_logs import BPI13ClosedProblems
+
+bpi13 = BPI13ClosedProblems()
+
