736c3c2044c50b85620e0b2f72d291c9a8819a90
seidi <rso.seidi@gmail.com>
2025-09-29 15:29:02 +0200

    refactor: index encoding works for multiple attributes

--- src/skpm/sequence_encoding/index.py
+++ src/skpm/sequence_encoding/index.py
@@ -1,39 +1,66 @@
-from skpm.config import EventLogConfig as elc
-from sklearn.base import TransformerMixin, _fit_context
-from skpm.base import BaseProcessEstimator
-from sklearn.utils._param_validation import Interval, _IterablesNotString, Options
 from numbers import Integral, Real
-import numpy as np
+
 import pandas as pd
-from typing import Union
+from sklearn.base import TransformerMixin
+from sklearn.utils._param_validation import Interval
+
+from skpm.base import BaseProcessEstimator
+from skpm.config import EventLogConfig as elc
+pd.options
 
 class Indexing(TransformerMixin, BaseProcessEstimator):
     _parameter_constraints = {
-            "n": [Interval(type=Integral, left=1, right=None, closed="left"), None],
-            "attributes": [str, list, None],
-            "fill_value": [Real, None],
+        "n": [Interval(type=Integral, left=1, right=None, closed="left"), None],  # type: ignore
+        "attributes": [str, list, None],
+        "fill_cat_value": [int, str, None],
+        "fill_num_value": [Real, None],
     }
-    def __init__(self, n: int = 2, attributes: Union[str, list] = None, fill_value: int = None):
+
+    def __init__(
+        self,
+        n: int | None = 2,
+        attributes: str | list[str] | None = None,
+        fill_cat_value: int | str | None = None,
+        fill_num_value: float | None = None,
+    ):
         self.n = n
         self.attributes = attributes
-        self.fill_value = fill_value
-
-    @_fit_context(prefer_skip_nested_validation=True)
-    def fit(self, X: pd.DataFrame, y=None):
-        if isinstance(self.attributes, str):
-            self.attributes = [self.attributes]
-        
-        if self.attributes is None:
-            self.attributes = X.columns.difference([elc.case_id]).tolist()
+        self.fill_cat_value = fill_cat_value
+        self.fill_num_value = fill_num_value
+    
+    def fit(self, X, y=None):
+        self._validate_params()  # type: ignore
         return self
 
-    def transform(self, X: pd.DataFrame, y=None):
-        group = X.groupby(elc.case_id)
+    def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:        
+        if self.attributes is None:
+            self.attributes = X.columns.difference([elc.case_id]).tolist()
+        elif isinstance(self.attributes, str):
+            self.attributes = [self.attributes]
         
+        group = X.groupby(elc.case_id)
+
         out_df = pd.DataFrame()
-        lags = range(self.n)
+        if self.n is not None:
+            lags = range(1, self.n + 1)
+        else:
+            lags = range(1, group.size().max())
+
+        num_attributes = X.select_dtypes(include=float)
+        time_attributes = X.select_dtypes(include=['datetime', 'timedelta', 'datetimetz'])
+        cat_attributes = X.select_dtypes(exclude=[float, 'datetime', 'timedelta', 'datetimetz'])
+
         for col in self.attributes:
-            lagged_cols = [f"{col}_lag_{lag}" for lag in lags]
-            out_df[lagged_cols] = group[col].shift(lags, fill_value=self.fill_value)
+            if col in num_attributes:
+                fill_value = self.fill_num_value
+            elif col in cat_attributes:
+                fill_value = self.fill_cat_value
+            elif col in time_attributes:
+                fill_value = None
                 
+            lagged_cols = [f"{col}_pos_{lag}" for lag in lags]
+            out_df[lagged_cols] = group[col].shift(
+                lags, fill_value=fill_value
+            )
+
         return out_df
