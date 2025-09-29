80072253df898f370a4beff5582066a951bbf7a8
seidi <rso.seidi@gmail.com>
2025-09-29 15:29:14 +0200

    test: index encoding

--- tests/sequence_encoding/test_index.py
+++ tests/sequence_encoding/test_index.py
@@ -1,34 +1,49 @@
-import polars as pl
-import pytest
+import warnings
+
 import numpy as np
 import pandas as pd
-from skpm.sequence_encoding import Indexing
+import pytest
+
 from skpm.config import EventLogConfig as elc
+from skpm.sequence_encoding import Indexing
 
 
 @pytest.fixture(name="pd_df")
 def fixture_dummy_pd():
+    n=1000
+    start = pd.Timestamp("2000-01-01")
+    end = pd.Timestamp("2024-01-01")
+    timestamps = pd.to_datetime(
+        np.linspace(start.value, end.value, n).astype(np.int64)
+    )
+
     return pd.DataFrame(
         {
-            elc.case_id: np.repeat(np.arange(0, 10), 100),
-            elc.activity: np.random.randint(0, 10, 1000),
-            elc.resource: np.random.randint(0, 3, 1000),
+            elc.case_id: np.repeat(np.arange(0, 10), int(n/10)),
+            elc.activity: np.random.randint(0, 10, n).astype(str),
+            elc.resource: np.random.rand(n),
+            elc.timestamp: timestamps
         }
     )
 
 def test_indexing(pd_df):
-    # Test default Indexing
-    rp = Indexing(n=2, attributes=[elc.activity, elc.resource], fill_value=0)
+    rp = Indexing()
     rp.fit(pd_df)
     out = rp.transform(pd_df)
     assert isinstance(out, pd.DataFrame)
     assert out.shape[0] == pd_df.shape[0]
-
-    with pytest.raises(Exception):
-        rp.transform(pd_df[[elc.activity, elc.resource]])
-
-    rp = Indexing(n=2, attributes=elc.activity, fill_value=0)
-    rp.fit(pd_df)
     
-    rp = Indexing(n=2, attributes=None, fill_value=0)
-    rp.fit(pd_df)
+    with pytest.raises(Exception):
+        rp = Indexing(n=0)
+        rp.fit(pd_df)
+        
+    rp = Indexing(n=2, fill_cat_value="TEST", fill_num_value=-1)
+    rp.fit_transform(pd_df)
+    
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", pd.errors.PerformanceWarning)
+        rp = Indexing(n=None)
+        rp.fit_transform(pd_df)
+    
+    rp = Indexing(n=None, attributes="concept:name")
+    rp.fit_transform(pd_df)
